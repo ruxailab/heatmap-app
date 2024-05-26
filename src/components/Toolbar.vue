4251f96f159b208aa2898ab1b9db5b557b753103
vGerJ02 <gjaimejuanguardia@gmail.com>
2024-05-26 20:50:22 +0200

    refactor: electronAPI safe calls and add hover text on url title

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -9,11 +9,15 @@
     <v-btn icon @click="goHome">
       <v-icon>mdi-home</v-icon>
     </v-btn>
-    <p class="font-weight-bold url-text">{{ this.url }}</p>
+    <p class="font-weight-bold url-text" :title="url">
+      {{ truncatedUrl }}
+    </p>
+    <v-spacer></v-spacer>
   </v-app-bar>
 </template>
 
 <script>
+const MAX_URL_LENGTH = 30
 export default {
   data() {
     return {
@@ -21,33 +25,40 @@ export default {
     }
   },
   created() {
-    if (window.electronAPI) {
-      window.electronAPI.on('url-updated', (value) => {
+    const { electronAPI } = window
+    if (electronAPI) {
+      electronAPI.on('url-updated', (value) => {
         this.url = value
       })
     }
   },
   computed: {
+    electronAPI() {
+      return window.electronAPI
+    },
     truncatedUrl() {
-      return this.url.length > 20 ? this.url.substring(0, 17) + '...' : this.url
+      return this.url.length > MAX_URL_LENGTH
+        ? this.url.substring(0, MAX_URL_LENGTH - 3) + '...'
+        : this.url
     },
   },
   methods: {
     goBack() {
-      if (window.electronAPI) window.electronAPI.send('backAction')
+      if (this.electronAPI) this.electronAPI.send('backAction')
       else console.log('electronAPI not defined in toolbar')
     },
     goForward() {
-      if (window.electronAPI) window.electronAPI.send('forwardAction')
+      if (this.electronAPI) this.electronAPI.send('forwardAction')
       else console.log('electronAPI not defined in toolbar')
     },
     goHome() {
-      if (window.electronAPI) window.electronAPI.send('resetURL')
+      if (this.electronAPI) this.electronAPI.send('resetURL')
       else console.log('electronAPI not defined in toolbar')
     },
   },
 }
 </script>
+
 <style>
 .url-text {
   position: absolute;
